64e8c6cae841d34c905e8dd3a12a385d2b2ab519
tiagosb <tdsb.contato@gmail.com>
2021-01-24 21:11:16 -0300

    Entendendo metodos em Ruby

---
+++ metodos.rb
@@ -0,0 +1,24 @@
+def soma(a, b)
+  c = a + b
+  return c
+end
+
+a, b = 5, 10
+c = soma(a, b)
+puts "#{a} + #{b} = #{c}"
+
+#Passando n argumentos para um método
+
+def exibe(*args)
+  args.each { 
+    |item| puts item
+  }
+end 
+
+exibe("Banana")
+exibe("Manga", "Uva")
+
+#É possível dar um apelido para o método; pra que isso???
+alias calcula soma
+puts calcula(5, 5)
+
